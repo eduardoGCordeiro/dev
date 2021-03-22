e3ee2afbe771f297e172b4c50659bab94b84b911
eduardoGCordeiro <eduardo@websix.com.br>
2021-03-22 11:38:53 -0300

    Ajuste nos id das tabelas.

--- database/migrations/2014_10_10_000000_create_education_levels_table.php
+++ database/migrations/2014_10_10_000000_create_education_levels_table.php
@@ -14,7 +14,7 @@ class CreateEducationLevelsTable extends Migration
     public function up()
     {
         Schema::create('education_levels', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('name');
 
             $table->softDeletes();
